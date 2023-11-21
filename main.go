173462ec6395c491f46b9232ce4285692c5b7e1b
jianghao <jianghao@haplox.com>
2023-11-21 08:58:34 +0800

    fix: change main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 )
 
 func main() {
@@ -15,6 +16,10 @@ func main() {
 	app.Get("/:name/:age/:gender?", func(c *fiber.Ctx) error {
 		msg := fmt.Sprintf("Hello %s, %s years old, %s", c.Params("name"), c.Params("age"), c.Params("gender"))
 		return c.SendString(msg)
+	})
+	app.Use(func(c *fiber.Ctx) error {
+		basicauth.New(basicauth.Config{})
+		return c.Next()
 	})
 	app.Listen(":3800")
 }
